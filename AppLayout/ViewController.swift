5e734859b8f0c8bce721e7a3b9a9e68837b4e336
mac <mac@macbook-pro.local>
2019-04-07 13:48:10 -0300

    Initial Commit

---
+++ AppLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLayout
+//
+//  Created by mac on 07/04/19.
+//  Copyright © 2019 Wesley S. Favarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
